a068f443e6d0aa00da78928ba33e9c0a0cb07d7a
Samarth Shinde <samarthsshinde@gmail.com>
2024-09-22 12:24:22 +0530

    3. property card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import { InfoBoxes } from "@/components/InfoBoxes";
+import { PropertyCard } from "@/components/PropertyCard";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<InfoBoxes />
+
 			<Footer />
 		</>
 	);
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,3 +1,20 @@
+import { PropertyCard } from "@/components/PropertyCard";
+import properties from "@/properties.json";
+
 export default function PropertyPage() {
-	return <>properties page</>;
+	return (
+		<section className="px-4 py-6">
+			<div className="container-xl lg:container m-auto px-4 py-6">
+				{properties.length === 0 ? (
+					<p>No properties found</p>
+				) : (
+					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+						{properties.map((property, index) => (
+							<PropertyCard key={index} property={property} />
+						))}
+					</div>
+				)}
+			</div>
+		</section>
+	);
 }
