b378c45da3a0189519550a9691a04f49542d8aed
Samarth Shinde <samarthsshinde@gmail.com>
2024-09-23 19:55:27 +0530

    19. profile page properties listing

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import profileDefault from "@/assets/images/profile.png";
 import { getSessionUser } from "@/utils/getSessionUser";
 import Image from "next/image";
+import Property from "@/models/Property";
+import { ProfileProperty } from "@/components/ProfileProperty";
 
 export default async function ProfilePage() {
 	await connectDB();
@@ -12,6 +14,8 @@ export default async function ProfilePage() {
 	if (!userId) {
 		throw new Error("User ID is required");
 	}
+
+	const properties = await Property.find({ owner: userId }).lean();
 
 	return (
 		<section className="bg-blue-50">
@@ -42,60 +46,9 @@ export default async function ProfilePage() {
 
 						<div className="md:w-3/4 md:pl-4">
 							<h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-							<div className="mb-10">
-								<Link href="/properties">
-									<img
-										className="h-32 w-full rounded-md object-cover"
-										src="/images/properties/a1.jpg"
-										alt="Property 1"
-									/>
-								</Link>
-								<div className="mt-2">
-									<p className="text-lg font-semibold">Property Title 1</p>
-									<p className="text-gray-600">Address: 123 Main St</p>
-								</div>
-								<div className="mt-2">
-									<Link
-										href="/properties/add"
-										className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-									>
-										Edit
-									</Link>
-									<button
-										className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-										type="button"
-									>
-										Delete
-									</button>
-								</div>
-							</div>
-							<div className="mb-10">
-								<Link href="/properties">
-									<img
-										className="h-32 w-full rounded-md object-cover"
-										src="/images/properties/b1.jpg"
-										alt="Property 2"
-									/>
-								</Link>
-								<div className="mt-2">
-									<p className="text-lg font-semibold">Property Title 2</p>
-									<p className="text-gray-600">Address: 456 Elm St</p>
-								</div>
-								<div className="mt-2">
-									<Link
-										href={`/properties/edit/6565}`}
-										className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-									>
-										Edit
-									</Link>
-									<button
-										className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-										type="button"
-									>
-										Delete
-									</button>
-								</div>
-							</div>
+							{properties.map((property) => (
+								<ProfileProperty property={property} key={property._id} />
+							))}
 						</div>
 					</div>
 				</div>
