85eab3ca3054425384cb1a6458758d52c6cd58cc
Samarth Shinde <samarthsshinde@gmail.com>
2024-09-24 19:27:31 +0530

    29. display search results

--- app/properties/search-results/page.tsx
+++ app/properties/search-results/page.tsx
@@ -1,7 +1,10 @@
 import { PropertyCard } from "@/components/PropertyCard";
+import { PropertySearchForm } from "@/components/PropertySearchForm";
 import { connectDB } from "@/config/database";
 import Property from "@/models/Property";
 import { convertToSerializeableObject } from "@/utils/convertToObject";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 interface searchParamsTypes {
 	location: string;
@@ -39,18 +42,33 @@ export default async function SearchPropertyPage({
 	const properties = convertToSerializeableObject(propertiesQueryResults);
 
 	return (
-		<section className="px-4 py-6">
-			<div className="container-xl lg:container m-auto px-4 py-6">
-				{properties.length === 0 ? (
-					<p>No properties found</p>
-				) : (
-					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-						{properties.map((property, index) => (
-							<PropertyCard property={property} key={index} />
-						))}
-					</div>
-				)}
-			</div>
-		</section>
+		<>
+			<section className="bg-blue-700 py-4">
+				<div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+					<PropertySearchForm />
+				</div>
+			</section>
+			<section className="px-4 py-6">
+				<div className="container-xl lg:container m-auto px-4 py-6">
+					<Link
+						href="/properties"
+						className="flex items-center text-blue-500 hover:underline mb-3"
+					>
+						<FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Properties
+					</Link>
+
+					<h1 className="text-2xl mb-4">Search Results</h1>
+					{properties.length === 0 ? (
+						<p>No search results</p>
+					) : (
+						<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+							{properties.map((property) => (
+								<PropertyCard property={property} key={property._id} />
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</>
 	);
 }
